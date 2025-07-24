0d7ef1fb9eacb7ae258f5c9a7f5e1cd96b330401
Michael-752 <142226964+Michael-752@users.noreply.github.com>
2025-07-24 16:50:31 +0800

    Updated .gitignore

--- chatConfig.js
+++
@@ -1,8 +0,0 @@
-// I know this is in a public repository
-// Not a big secret
-// Just for development purposes
-
-export const chatApiKey = "dz5f4d5kzrue";
-export const chatUserId = "still-sun-4";
-export const chatUserName = "still";
-export const chatUserToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoic3RpbGwtc3VuLTQiLCJleHAiOjE3NTI0OTIwMDV9.FGWcbssd7iMaJjZS6tmraSxQcMQUYbD20UVWnVKSyJU";
